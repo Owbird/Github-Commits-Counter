1d70da9c91bc63e3d9fea4039972c86f02e3cc94
owbird <owbirdphorccuo@gmail.com>
2024-04-22 02:48:02 +0000

    feat: use Gh graphql api

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from bs4 import BeautifulSoup
 from fastapi import FastAPI, Query, Response
 from datetime import datetime, timedelta
-from requests import get
+from requests import get, post
+from os import getenv
+from dotenv import load_dotenv
 
 app = FastAPI()
 
 
 def get_total_commits(username: str) -> int:
     try:
+
+        load_dotenv()
 
         today = datetime.now()
 
@@ -17,38 +20,45 @@ def get_total_commits(username: str) -> int:
 
         end_date = next_month - timedelta(days=1)
 
-        start_date_formatted = start_date.strftime('%Y-%m-%d')
-        end_date_formatted = end_date.strftime('%Y-%m-%d')
+        start_date_formatted = start_date.strftime('%Y-%m-%dT%H:%M:%SZ')
+        end_date_formatted = end_date.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-        url = f"https://github.com/{username}?tab=overview&from={start_date_formatted}&to={end_date_formatted}"
+        url = "https://api.github.com/graphql"
 
-        response = get(url)
+        query = f"""
+                query {{
+                    user(login: "{username}") {{
+                        contributionsCollection(
+                        from: "{start_date_formatted}",
+                        to: "{end_date_formatted}") {{
+                            contributionCalendar {{
+                                totalContributions
+                                }}
+                            }}
+                        }}
+                }}
+        """
 
-        soup = BeautifulSoup(response.text, "html.parser")
+        headers = {"Authorization": f"Bearer {getenv("GH_KEY")}"}
 
-        total_days = (end_date - start_date).days
+        response = post(url,
+                        headers=headers,
+                        json={"query": query})
 
-        print(total_days)
+        data = response.json()
 
-        date = start_date
-
-        contributions = []
-
-        for _ in range(total_days + 1):
-            contributions.extend(soup.find_all(
-                attrs={"data-date": date.strftime("%Y-%m-%d")}))
-            date += timedelta(days=1)
-
-        total_commits = sum(int(contribution.text.split(" ")[
-            0]) for contribution in contributions if contribution.text.split(" ")[0] != "No")
-
-        return total_commits
+        return (
+            data["data"]["user"]["contributionsCollection"]
+            ["contributionCalendar"]["totalContributions"]
+        )
     except Exception as e:
+        print(e)
         return 0
 
 
 def get_badge(commits: int):
-    url = f"https://img.shields.io/badge/Total_Commits_This_Month-{commits}-green"
+    url = f"https://img.shields.io/badge/Total_Commits_This_Month-{
+        commits}-green"
 
     response = get(url)
 
